5fb026c2f441a76894ca18a38bce082c75a9814f
Aman Kumar Sinha <amankrsinha07@gmail.com>
2024-07-13 23:16:29 +0530

    Day 1

---
+++ Day 1/index.js
@@ -0,0 +1,44 @@
+// Task 1
+var num = 10;
+console.log(num);
+
+
+// Task 2
+let name = "Aman";
+console.log(name);
+
+
+// Task 3
+const isChallengeCompleted = true;
+console.log(isChallengeCompleted);
+
+
+// Task 4
+let day = 1;
+let course = "JavaScript";
+let completed = true;
+
+let details = {
+    day: 1,
+    course: "JavaScript",
+    completed: true
+}
+
+console.log(typeof(day));
+console.log(typeof(course));
+console.log(typeof(completed));
+console.log(typeof(details));
+
+
+// Task 5
+const pi = 3.14;
+
+// Task 6
+// pi = 4; // throw error
+
+
+let score = 100;
+score = 101;
+
+
+
